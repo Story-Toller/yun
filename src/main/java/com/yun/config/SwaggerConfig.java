2528fcb4d2f50052b4ef1aab7e5d0d4f4a1e6db9
story <1269355513@qq.com>
2021-09-18 09:48:18 +0800

    引入前端模板，书写配置类

---
+++ src/main/java/com/yun/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.yun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2  //开启swagger2
+public class SwaggerConfig {
+
+    @Bean
+//    配置了swagger的Docket的bean实例
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    //    配置swagger信息  apiInfo
+    private ApiInfo apiInfo() {
+//        作者信息
+        Contact contact = new Contact("yun", "https://github.com/Story-Toller/yun_project.git", "1269355513@qq.com");
+        return new ApiInfo(
+                "Swagger API For Yun",
+                "Swagger API For Yun",
+                "v1.0",
+                "https://yun.cn",
+                contact,
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
+    }
+
+}
---
+++ src/main/java/com/yun/config/WebConfig.java
@@ -0,0 +1,38 @@
+package com.yun.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //注册拦截器
+    //addPathPatterns 用来设置拦截路径
+    //excludePathPatterns 用来设置白名单，也就是不需要触发这个拦截器的路径。
+    //addPathPatterns("/**") 表示拦截所有的请求
+    //.excludePathPatterns("/login","/regist")表示出了登录与注册之外，都会拦截（因为登录注册不需要登录页可以访问）
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/company/**","/custmoer/center","/record/**","/resume/**","/job/**")
+                .excludePathPatterns("/job/getByPageForIndex");
+                                                  /*  .excludePathPatterns("/login")
+                                                    .excludePathPatterns("/regist");*/
+
+    }
+    //这个方法是用来配置静态资源的，比如html,js,css等
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","/index");
+        registry.addRedirectViewController("/job-list","/job-list");
+        registry.addRedirectViewController("/ChangePwd","/ChangePwd.jsp");
+
+    }
+}
